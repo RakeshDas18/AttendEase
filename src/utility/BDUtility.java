c3daea71cfe94310aa33544b1cd42bdba9873a91
Rakesh Das <18.rakeshdas@gmail.com>
2025-02-07 19:34:33 +0530

    Fix the JFrame dashboard visibility after successful login

--- src/utility/BDUtility.java
+++ src/utility/BDUtility.java
@@ -2,18 +2,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
+
 package utility;
 
-import forms.Dashboard;
+//import java.io.*; 
+
+import java.util.*;
+import javax.swing.*;
+
 
 /**
  *
  * @author LENOVO
  */
 public class BDUtility {
-
-    public static void openForm(String simpleName, Dashboard dashboard) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    
+    private static HashMap<String, JFrame> formsMap = new HashMap<>();
+    
+    public static void openForm(String formName, JFrame formInstance){
+        JFrame existingForm = formsMap.get(formName);
+        
+        if(existingForm == null || !existingForm.isVisible()){
+            formsMap.put(formName, formInstance);
+            formInstance.setVisible(true);
+        } else {
+            existingForm.toFront();
+        }
     }
     
+   
 }
